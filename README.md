559cc10012ce781d28d8dcbd51f624dd6d94e1b1
yurilov <yuraschepilov@gmail.com>
2021-12-24 14:34:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-6dwgbg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-6dwgbg)
---
+++ index.js
@@ -0,0 +1,39 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const objA = {
+  name: 'Yura',
+  age: 24,
+};
+
+
+
+const objB = {
+  ...objA,
+}
+const objC = objA;
+objB.name = 'Anya';
+objB.age = 22
+
+
+objC.name = 'roman';
+
+console.log(objC);
+console.log(objB);
+
+console.log(Object.entries(objC));
+
+
+const fnA = (...params) => params.map(n => n*n);
+
+console.log(fnA(1,2,3,4));
+
+const sectionHeading = document.querySelector('.section__heading');
+
+console.log(sectionHeading.innerHTML);
+
+sectionHeading.innerHTML = '<h1>WTF!!!!!</h2>';
