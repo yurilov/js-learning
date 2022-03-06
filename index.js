71b4dd810a64a3c027c6778498f651e0d2580568
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-03-06 10:01:35 +0200

    rearenged files

--- index.js
+++ index.js
@@ -1,39 +1,33 @@
-// Import stylesheets
-import './style.css';
+import "./css/style.css";
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
+const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 const objA = {
-  name: 'Yura',
+  name: "Yura",
   age: 24,
 };
 
-
-
 const objB = {
   ...objA,
-}
+};
 const objC = objA;
-objB.name = 'Anya';
-objB.age = 22
+objB.name = "Anya";
+objB.age = 22;
 
-
-objC.name = 'roman';
+objC.name = "roman";
 
 console.log(objC);
 console.log(objB);
 
 console.log(Object.entries(objC));
 
+const fnA = (...params) => params.map((n) => n * n);
 
-const fnA = (...params) => params.map(n => n*n);
+console.log(fnA(1, 2, 3, 4));
 
-console.log(fnA(1,2,3,4));
-
-const sectionHeading = document.querySelector('.section__heading');
+const sectionHeading = document.querySelector(".section__heading");
 
 console.log(sectionHeading.innerHTML);
 
-sectionHeading.innerHTML = '<h1>WTF!!!!!</h2>';
+sectionHeading.innerHTML = "<h1>WTF!!!!!</h2>";
